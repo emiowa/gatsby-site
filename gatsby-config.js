ea04b837bd26f0de241ece0ddd29bc21446e2b2c
Emi Owashi Vallejo <emiowa@hotmail.com>
2022-01-31 22:34:25 +0900

    Finished Gatsby Tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,16 @@
 module.exports = {
     siteMetadata: {
-        title: "Gatsby Site",
+        title: "The baby angel",
     },
     plugins: [
         "gatsby-plugin-image",
         "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+              name: `blog`,
+              path: `${__dirname}/blog`,
+            }
+        },
     ],
 };
